ebc64905c10475abad8dae7a3fc9d4518d543a4d
Yonatan Mizrahi <yoni9696@walla.com>
2021-02-06 23:37:52 +0200

    added modal

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,6 +1,7 @@
 'use strict'
 
 var map;
+var gLastPos;
 
 function init() {
     createMap();
@@ -31,14 +32,34 @@ function createMap(lat = 29.55805, lng = 34.94821) {
 
 function addMapEvents() {
 
+    var myloc = new google.maps.Marker({
+        clickable: false,
+        icon: new google.maps.MarkerImage('//maps.gstatic.com/mapfiles/mobile/mobileimgs2.png',
+            new google.maps.Size(22, 22),
+            new google.maps.Point(0, 18),
+            new google.maps.Point(11, 11)),
+        shadow: null,
+        zIndex: 999,
+        map: map
+    });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            var me = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+            myloc.setPosition(me);
+        }, function(error) {
+            // ...
+        });
+    }
+
     // get location
     var infoWindow = new google.maps.InfoWindow();
-    const locationButton = document.createElement("img");
-    locationButton.src = "../img/my-location1.png";
-    locationButton.classList.add("custom-map-control-button");
+    const locationButton = document.createElement('img');
+    locationButton.src = '../img/my-location1.png';
+    locationButton.classList.add('custom-map-control-button');
     map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
 
-    locationButton.addEventListener("click", () => {
+    locationButton.addEventListener('click', () => {
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
@@ -48,7 +69,7 @@ function addMapEvents() {
                         lng: position.coords.longitude,
                     };
                     infoWindow.setPosition(pos);
-                    infoWindow.setContent("Your Location");
+                    infoWindow.setContent('Your Location');
                     infoWindow.open(map);
                     map.panTo(pos);
                 },
@@ -62,36 +83,24 @@ function addMapEvents() {
         }
     });
 
-    // listens for clicking the map event, getting the coords
+    // // listens for clicking the map event, getting the coords
 
-    map.addListener("click", (mapsMouseEvent) => {
+    map.addListener('click', (mapsMouseEvent) => {
         // Close the current InfoWindow.
         infoWindow.close();
         // Create a new InfoWindow.
         infoWindow = new google.maps.InfoWindow({
             position: mapsMouseEvent.latLng,
         });
-
-        saveLocation(mapsMouseEvent.latLng.toJSON(), null, 2);
-
-        // save location to storage
-        // saveLocation(JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2);
-
-        // infoWindow.setContent(
-        //     JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-        // );
-        // infoWindow.open(map);
+        // saveLocation(mapsMouseEvent.latLng.toJSON(), null, 2);
+        gLastPos = mapsMouseEvent.latLng.toJSON();
+        console.log(gLastPos);
+        $('#input-modal').modal('show');
     });
+
+
 }
 
-
-
-function saveLocation(pos) {
-    var newName = prompt('Place name?');
-    placeMarker(newName, pos);
-    savePlace(pos.lat, pos.lng, newName);
-    renderPlaces();
-}
 
 
 function placeMarker(newName, position) {
@@ -107,20 +116,20 @@ function placeMarker(newName, position) {
 
 
 function handleLocationError(error) {
-    var locationError = document.getElementById("locationError");
+    var locationError = document.getElementById('locationError');
 
     switch (error.code) {
         case 0:
-            locationError.innerHTML = "There was an error while retrieving your location: " + error.message;
+            locationError.innerHTML = 'There was an error while retrieving your location: ' + error.message;
             break;
         case 1:
-            locationError.innerHTML = "The user didn't allow this page to retrieve a location.";
+            locationError.innerHTML = 'The user didn\'t allow this page to retrieve a location.';
             break;
         case 2:
-            locationError.innerHTML = "The browser was unable to determine your location: " + error.message;
+            locationError.innerHTML = 'The browser was unable to determine your location: ' + error.message;
             break;
         case 3:
-            locationError.innerHTML = "The browser timed out before retrieving the location.";
+            locationError.innerHTML = 'The browser timed out before retrieving the location.';
             break;
     }
 }
@@ -128,7 +137,6 @@ function handleLocationError(error) {
 
 function goToLocation(lat, lng) {
     var position = { lat, lng };
-    console.log(position);
     map.panTo(position);
 }
 
@@ -147,10 +155,10 @@ function renderPlaces() {
     var strHtmls = places.map((place) => {
         var position = { lat: place.lat, lng: place.lng };
         // console.log(position);
-        return `<tr onclick="goToLocation(${place.lat}, ${place.lng})"><td data-id="place.id" > ${place.id}</td>
-        <td>${place.placeName}</td>
-        <td class="delete"><button class="delete btn btn-outline-danger" onclick="deletePlace(${place.id})">Delete</button></td>
-        </tr>`;
+        return `<tr onclick='goToLocation(${place.lat}, ${place.lng})'><td data-id='place.id' > ${place.id}</td>
+                        <td>${place.placeName}</td>
+                        <td class='delete'><button class='delete btn btn-outline-danger' onclick='deletePlace(${place.id})'>Delete</button></td>
+                        </tr>`;
 
     }).join('');
 
@@ -167,9 +175,26 @@ function deletePlace(id) {
 
 function setUserPerf() {
     var userData = getUserData();
+    if (!userData) return;
     var elHeader = document.querySelector('.topnav');
 
     var elTitle = document.querySelector('.active');
     elHeader.style.backgroundColor = userData.backgroundColor;
     elTitle.style.color = userData.textColor;
+}
+
+function onSubmitForm(ev) {
+    ev.preventDefault();
+    saveLocation(gLastPos);
+    $('#input-modal').modal('hide');
+}
+
+
+function saveLocation(pos) {
+    var newName = document.querySelector('[name="place"]').value;
+    placeMarker(newName, pos);
+    savePlace(pos.lat, pos.lng, newName);
+    gLastPos = null;
+    document.querySelector('[name="place"]').value = '';
+    renderPlaces();
 }
